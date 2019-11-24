37f532372c10c1c8695eeb5998c58bb1c8d0ecb9
kyuha023 <48377955+kyuha023@users.noreply.github.com>
2019-11-25 02:25:01 +0900

    audio 출력
    audio 출력

--- audio.py
+++ audio.py
@@ -1,9 +1,15 @@
 import speech_recognition as sr
+from gtts import gTTS
+import os
 
 r=sr.Recognizer()
 
-mic =sr.Microphone()
-
-with mic as source:
-    audio = r.listen(source)
-print(r.recognize_google(audio,language='ko-KR'))
+mic = sr.Microphone()
+while True:
+    with mic as source:
+        audio = r.listen(source)
+        nameString = "안녕하세요"
+        tts = gTTS(text=nameString, lang='ko')
+        tts.save('my.mp3')
+        os.system('my.mp3')
+    print(r.recognize_google(audio,language='ko-KR'))
