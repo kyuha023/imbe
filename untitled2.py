569a8763652569863ef0f687e00530e163b10070
kyuha023 <48377955+kyuha023@users.noreply.github.com>
2019-12-11 03:25:19 +0900

    change main code
    change main code

--- untitled2.py
+++ untitled2.py
@@ -18,7 +18,7 @@ def Message():
     utterance = content['utterance']
     if utterance == "자리 있어?" :
         camera = picamera.PiCamera()
-        camera.resolution = (800, 600)
+        camera.resolution = (600, 800)
         camera.capture('ex1.jpg')
         camera.close()
         come='curl -X POST -u "apikey:rTPcDZUHnegT9yLjj3iGNABlYYk0JToqzf7T2kWf24n0" -F "features=objects" -F "collection_ids=64e568a1-96c1-488a-b26c-8c61c6d95f0a" -F "images_file=@myimg.jpg" "https://gateway.watsonplatform.net/visual-recognition/api/v4/analyze?version=2019-02-11"';
@@ -29,11 +29,10 @@ def Message():
         re.search("location",mygg)
         len(regex.findall(mygg))
 
-
     elif utterance == "자리 생기면 알려줘" :
         while True:
             camera = picamera.PiCamera()
-            camera.resolution = (800, 600)
+            camera.resolution = (600, 800)
             camera.capture('ex1.jpg')
             camera.close()
             come='curl -X POST -u "apikey:rTPcDZUHnegT9yLjj3iGNABlYYk0JToqzf7T2kWf24n0" -F "features=objects" -F "collection_ids=64e568a1-96c1-488a-b26c-8c61c6d95f0a" -F "images_file=@myimg.jpg" "https://gateway.watsonplatform.net/visual-recognition/api/v4/analyze?version=2019-02-11"';
