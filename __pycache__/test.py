e0b03c9bb6b6ff4502708e527eabf6e2ffe9a0e6
Codingtree34 <54171914+Codingtree34@users.noreply.github.com>
2019-12-12 10:09:26 +0900

    Add files via upload

---
+++ __pycache__/test.py
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>Form</class>
+ <widget class="QWidget" name="Form">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>397</width>
+    <height>300</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Form</string>
+  </property>
+  <widget class="QWidget" name="verticalLayoutWidget">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>401</width>
+     <height>301</height>
+    </rect>
+   </property>
+   <layout class="QVBoxLayout" name="verticalLayout">
+    <item>
+     <widget class="QLabel" name="label">
+      <property name="text">
+       <string>TextLabel</string>
+      </property>
+     </widget>
+    </item>
+    <item alignment="Qt::AlignBottom">
+     <widget class="QPushButton" name="PushButton">
+      <property name="text">
+       <string>clear</string>
+      </property>
+     </widget>
+    </item>
+    <item>
+     <widget class="QLineEdit" name="lineEdit"/>
+    </item>
+   </layout>
+  </widget>
+ </widget>
+ <resources/>
+ <connections>
+  <connection>
+   <sender>PushButton</sender>
+   <signal>clicked()</signal>
+   <receiver>lineEdit</receiver>
+   <slot>clear()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>219</x>
+     <y>288</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>229</x>
+     <y>260</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>PushButton</sender>
+   <signal>clicked()</signal>
+   <receiver>label</receiver>
+   <slot>clear()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>273</x>
+     <y>289</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>283</x>
+     <y>180</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>lineEdit</sender>
+   <signal>textChanged(QString)</signal>
+   <receiver>label</receiver>
+   <slot>setText(QString)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>149</x>
+     <y>252</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>150</x>
+     <y>168</y>
+    </hint>
+   </hints>
+  </connection>
+ </connections>
+</ui>
+///dfg
+
+
+s/sdf/g/sdfg
+
+
+sd/f
+gsd/fgsd
+fg
+sdfg
+sdf
+
+
+sdfg/dfsg/sd/f//sdfgsd
+f
+sdfg/sdf/gsfg/
+s
+s
+dfg
+s
+dfg
+sd
+f
+sdfg///
+sdf
+g
+sdf
+g
+sdf/sdfgsd
+g
+s
+df
+g
+sdfg
+sdfg
+sdf
+g/sd
+fg/sd
+g
+sd/fg
+sd
+fg
+sd/g/s
+dfg
+/sd
+/fg
+/sdfgsd
